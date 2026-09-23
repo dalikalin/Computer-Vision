ba9b11cba9d8ae38f65d11a8c0ea6d6e17153f67
DalikaLin <lindalika.kh@gmail.com>
2026-09-23 20:17:43 +0700

    Lab1

--- Lab2 ImageTransformation/Part1 Basic Image Transformation/Exercise2_TranslateanImage.py
+++ Lab2 ImageTransformation/Part1 Basic Image Transformation/Exercise2_TranslateanImage.py
@@ -5,6 +5,24 @@
 # o Shift an image 100 pixels to the right and 50 pixels down using cv2.warpAffine().
 # o Save the image.
 import cv2
+import numpy as np
 
 #Load the image
 img = cv2.imread("sheldon.jpg")
+
+h, w = img.shape[:2]
+
+# Define the translation matrix (2x3)[1, 0, tx][0, 1, ty]
+# shift right by 100, down by 50
+translationmatrix = np.float32([[1, 0, 100], [0, 1, 50]])
+
+# Apply the translation
+translatedimage = cv2.warpAffine(img, translationmatrix, (w, h))
+
+#Display the image
+cv2.imshow("translatedimage", translatedimage)
+#Save the image
+cv2.imwrite("translatedsheldon.jpg", translatedimage)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
